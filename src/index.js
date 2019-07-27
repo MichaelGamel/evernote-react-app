265dc421d2572d15ac1175cce858361665d9796d
Michael <michael.gamel@hotmail.com>
2019-07-28 03:57:10 +0400

    add env variables

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ require('firebase/firestore');
 
 // Initialize Firebase
 firebase.initializeApp({
-  apiKey: '<api-key>',
-  authDomain: '<auth-domain>',
-  databaseURL: '<database-url>',
-  projectId: '<project-id>',
-  storageBucket: '<storage-bucket>',
-  messagingSenderId: '<messaging-sender-id>',
-  appId: '<app-id>'
+  apiKey: REACT_APP_FIREBASE_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  databaseURL: REACT_APP_DATABASE_URL,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_STORAGE_ID,
+  appId: REACT_APP_APP_ID
 });
 
 ReactDOM.render(<App />, document.getElementById('evernote-container'));
